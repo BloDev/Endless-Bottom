e26e1c77c5a6951c10fe243485d5815b76712411
Calvin Yuen <calvinyk0@gmail.com>
2020-09-08 19:28:02 -0400

    Create parallax script

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public GameObject target; 
+    private float startPos; 
+    private float height; 
+    public float parallaxEffect; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.y; 
+        // height = GetComponent<SpriteRenderer>().bounds.size.y; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = target.transform.position.y * parallaxEffect;
+        transform.position = new Vector3(transform.position.x, startPos - distance, transform.position.z);  
+    }
+}
