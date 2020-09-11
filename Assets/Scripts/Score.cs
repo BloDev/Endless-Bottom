4e192965758762ac1d26e3c46b855bfe8ca489a8
Brandon Lo <46232632+BloDev@users.noreply.github.com>
2020-09-10 23:08:19 -0400

    Add score script

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D obj) {
+        if (obj.CompareTag("Player")) {
+            Destroy(obj.gameObject);
+        }
+    }
+}
