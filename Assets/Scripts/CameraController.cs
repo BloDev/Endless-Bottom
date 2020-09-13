ad35cf22045f6a4eb375437cde022e32b42363c9
Brandon Lo <46232632+BloDev@users.noreply.github.com>
2020-09-13 05:56:22 -0400

    Add camera script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera.main.orthographicSize = (cameraWidth / 2) / Camera.main.aspect;
+    }
+}
