a349f799e6537918e09733e59d26ad77de542587
Brandon Lo <46232632+BloDev@users.noreply.github.com>
2020-09-14 02:09:38 -0400

    Fix camera setup script

---
+++ Assets/Scripts/CameraSetup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetup : MonoBehaviour
+{
+    public BoxCollider2D top;
+    public BoxCollider2D bottom;
+    public BoxCollider2D left;
+    public BoxCollider2D right;
+
+    void Start()
+    {
+        Camera.main.orthographicSize = (Camera.main.orthographicSize / 2) / Camera.main.aspect;
+
+        top.size = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f);
+        top.offset = new Vector2(0f, Camera.main.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y + 0.5f);
+
+        bottom.size = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f);
+        bottom.offset = new Vector2(0f, Camera.main.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y - 0.5f);
+
+        left.size = new Vector2(1f, Camera.main.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y);
+        left.offset = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f, 0f);
+
+        right.size = new Vector2(1f, Camera.main.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y);
+        right.offset = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
+    }
+}
