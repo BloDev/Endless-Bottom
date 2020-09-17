16370ebb01d56366e7f7e82c3b52776409292264
Calvin Yuen <calvinyk0@gmail.com>
2020-09-17 01:15:57 -0400

    Update parallax script

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -5,17 +5,9 @@ using UnityEngine;
 public class Parallax : MonoBehaviour {
 
     public float endY; 
-    public float startY;
     private float backgroundSpeed; 
 
     void Update() {   
-        /*
-        transform.Translate(Vector2.up * backgroundSpeed * Time.deltaTime);
-        
-        if (transform.position.y >= endY) {
-            transform.position = new Vector2(transform.position.x, endY * -1); 
-        }
-        */
         backgroundSpeed = GameObject.Find("Spawner").GetComponent<PlatformSpawner>().platformSpeed;
 
         foreach (Transform child in transform) {
