94ffa618f405d254697c1940e5bf612396d0e753
Calvin Yuen <calvinyk0@gmail.com>
2020-12-28 13:15:10 -0500

    Update a line of code in Parallax

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -15,7 +15,7 @@ public class Parallax : MonoBehaviour {
             if (child.position.y >= endY) {
                 child.position = new Vector3(child.position.x, endY * -1, child.position.z); 
             }
-            backgroundSpeed = backgroundSpeed / (float)1.5; 
+            backgroundSpeed = backgroundSpeed / 1.5f; 
         }
     }
 }
