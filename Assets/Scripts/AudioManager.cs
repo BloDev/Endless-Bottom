044d0536fa5ba3c9a45f54500190020668afb446
Calvin Yuen <calvinyk0@gmail.com>
2021-01-08 03:05:30 -0500

    Craete "AudioManager" script that is used to play sound effects in game

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds; 
+
+    void Awake()
+    {
+        foreach (Sound sound in sounds) {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    void Start() 
+    {
+        Play("Background Music");
+    }
+
+    public void Play(string name)
+    {
+        foreach (Sound sound in sounds) {
+            if (String.Equals(sound.name, name)) {
+                sound.source.Play();
+            }
+        }
+    }
+}
