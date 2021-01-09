9c2be954e3ae83ba377b0d9576b72bc8a1a90acc
Calvin Yuen <calvinyk0@gmail.com>
2021-01-08 20:44:56 -0500

    Import new sound effects and replace player death sound effect

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -10,16 +10,19 @@ public class Main : MonoBehaviour
     public GameObject pauseMenu;
 
     public void Pause() { 
+        FindObjectOfType<AudioManager>().Play("Pause Sound");
         Time.timeScale = 0f;
         pauseMenu.SetActive(true);
     }
 
     public void Resume() {
+        FindObjectOfType<AudioManager>().Play("Unpause Sound");
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
     }
 
     public void MainMenu() {
+        FindObjectOfType<AudioManager>().Play("Button Clicking Sound");
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
