8e294e745505f627708df74272247a4fd504a959
Calvin Yuen <calvinyk0@gmail.com>
2021-01-09 01:38:06 -0500

    Add AudioManager to all scenes and update script so that it uses singleton design

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,8 +7,19 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds; 
+    public static AudioManager audioManager;
 
     void Awake() {
+
+        if (audioManager == null) {
+            audioManager = this;
+        }
+        else {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
         foreach (Sound sound in sounds) {
             sound.source = gameObject.AddComponent<AudioSource>();
             sound.source.clip = sound.clip;
